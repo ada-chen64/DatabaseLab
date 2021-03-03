728bd9f268727744890d918ef360bf4503b40e89
rcy <rcycyzh@163.com>
2021-03-03 21:43:39 +0800

    Add json output

--- test.sh
+++ test.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 : ${LAB_UNTIL:=1}
+: ${SEND_RESULT:=0}
 
 mkdir build 
 cd build 
 cmake ..
 make
 
+echo current project ${CI_PROJECT_NAME}
+echo pipeline id ${CI_PIPELINE_ID}
 
 for (( lab = 1; lab <= ${LAB_UNTIL}; lab++))
 do
     echo "Testing Lab${lab}"
-    ./lab${lab}_test --gtest_break_on_failure || exit 1
+    bin/thdb_init
+    bin/lab${lab}_test --gtest_output="json" --gtest_brief=1 || fail=1
+    bin/thdb_clear
 done
+
+if [ $SEND_RESULT -eq 1 -a -e report.json ]
+then
+    echo finish send result here
+fi
+
+if [ $fail -eq 1]
+then
+    exit 1
+fi
  
 echo ALL PASSED
