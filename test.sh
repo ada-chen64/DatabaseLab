d6f2586558549bc60dd77851b53ffb450e6cd6aa
rcy <rcycyzh@163.com>
2021-03-04 10:15:13 +0800

    Remove bin/thdb_init call

--- test.sh
+++ test.sh
@@ -12,17 +12,14 @@ echo -e current project "\x1b[32m${CI_PROJECT_NAME}\x1b[0m"
 echo -e pipeline id "\x1b[32m${CI_PIPELINE_ID}\x1b[0m"
 
 echo -e "\x1b[1;32mStarting test Lab${CURRENT_LAB}\x1b[0m"
-fail=0
-bin/thdb_init
 test/lab${CURRENT_LAB}_test --gtest_output="json" --gtest_brief=1 || fail=1
-bin/thdb_clear
 
 if [ $SEND_RESULT -eq 1 -a -e report.json ]
 then
     echo finish send result here
 fi
 
-if [ $fail -eq 1 ]
+if [ ${fail:-0} -eq 1 ]
 then
     exit 1
 fi
