de0f495d0765f692f0ca4f6d57da0dfdd49dc74b
rcy <rcycyzh@163.com>
2021-03-04 10:20:55 +0800

    Debug

--- test/lab1/main.cc
+++ test/lab1/main.cc
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
+#include "backend/backend.h"
 #include "gtest/gtest.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
+    thdb::Init();
     auto res = RUN_ALL_TESTS();
-    return res;
+    printf("Write Back Begin;\n");
+    thdb::Close();
+    printf("Write Back End;\n");
+    return 0;
 }
