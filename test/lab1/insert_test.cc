631935e844f253d9703bcb276ef7c5a63ab6a2ac
董昊文 <dhw17@mails.tsinghua.edu.cn>
2021-03-04 15:29:57 +0800

    Fix a bug

--- test/lab1/insert_test.cc
+++ test/lab1/insert_test.cc
@@ -8,7 +8,7 @@ TEST(Lab1, InsertTest) {
   Instance *pDB = new Instance();
   std::vector<String> iSQLVec = {
       "SHOW TABLES;", "CREATE TABLE A(A INT, B FLOAT, C VARCHAR(30), D FLOAT);",
-      "SHOW TABLES;", "INSERT INTO A VALUES(4, 4.5, test_string, 5.0)",
+      "SHOW TABLES;", "INSERT INTO A VALUES(4, 4.5, 'test_string', 5.0)",
       "DROP TABLE A;"};
   for (const auto &sSQL : iSQLVec) {
     Execute(pDB, sSQL);
