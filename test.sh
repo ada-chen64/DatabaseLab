b1d2fe9f35fe63b08be3ef19db1bc089fe982b82
rcy <rcycyzh@163.com>
2021-03-04 13:52:56 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-: ${CURRENT_LAB:=1}
+: ${CURRENT_LAB:=0}
 : ${SEND_RESULT:=0}
 
 mkdir build 
@@ -11,11 +11,13 @@ make -j 16 -l 32
 echo -e current project "\x1b[32m${CI_PROJECT_NAME}\x1b[0m"
 echo -e pipeline id "\x1b[32m${CI_PIPELINE_ID}\x1b[0m"
 
+if [ $CURRENT_LAB -eq 0 ]
+then
+    exit 0
+fi
+
 echo -e "\x1b[1;32mStarting test Lab${CURRENT_LAB}\x1b[0m"
-echo -e "test/lab${CURRENT_LAB}_test --gtest_output=\"json\" --gtest_brief=1 || fail=1"
-# echo -e "\x1b[1;31m"
 test/lab${CURRENT_LAB}_test --gtest_output="json" --gtest_color="yes" || fail=1
-# echo -e "\x1b[0m"
 
 if [ $SEND_RESULT -eq 1 -a -e report.json ]
 then
