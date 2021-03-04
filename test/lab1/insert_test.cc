8d1272d52980801d74bc091ddb07d599f828409d
董昊文 <dhw17@mails.tsinghua.edu.cn>
2021-03-04 16:57:32 +0800

    Add Test.

--- test/lab1/insert_test.cc
+++ test/lab1/insert_test.cc
@@ -7,8 +7,24 @@ namespace thdb {
 TEST(Lab1, InsertTest) {
   Instance *pDB = new Instance();
   std::vector<String> iSQLVec = {
-      "SHOW TABLES;", "CREATE TABLE A(A INT, B FLOAT, C VARCHAR(30), D FLOAT);",
-      "SHOW TABLES;", "INSERT INTO A VALUES(4, 4.5, 'test_string', 5.0)",
+      "SHOW TABLES;",
+      "CREATE TABLE A(A INT, B FLOAT, C VARCHAR(30), D FLOAT);",
+      "SHOW TABLES;",
+      "INSERT INTO A VALUES(4, 4.5, 'test_string', 5.0)",
+      "SELECT * FROM A;",
+      "SELECT * FROM A WHERE A.A = 4;",
+      "SELECT * FROM A WHERE A.A < 4;",
+      "SELECT * FROM A WHERE A.A > 4;",
+      "SELECT * FROM A WHERE A.A <= 4;",
+      "SELECT * FROM A WHERE A.A >= 4;",
+      "SELECT * FROM A WHERE A.A <> 4;",
+      "SELECT * FROM A WHERE A.A <> 3;",
+      "SELECT * FROM A WHERE A.A < 5;",
+      "SELECT * FROM A WHERE A.A > 3;",
+      "INSERT INTO A VALUES(5, 4.5, 'test_string', 5.0)",
+      "SELECT * FROM A;",
+      "DELETE FROM A WHERE A.A = 4;",
+      "SELECT * FROM A;",
       "DROP TABLE A;"};
   for (const auto &sSQL : iSQLVec) {
     Execute(pDB, sSQL);
