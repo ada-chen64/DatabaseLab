8da84117d0ddc2b28764f81dd54205ba6cd14a77
李文博 <li-wb17@mails.tsinghua.edu.cn>
2021-03-04 15:17:29 +0800

    Add table test and insert test

---
+++ test/lab1/insert_test.cc
@@ -0,0 +1,20 @@
+#include "backend/backend.h"
+#include "gtest/gtest.h"
+#include "system/instance.h"
+
+namespace thdb {
+
+TEST(Lab1, InsertTest) {
+  Instance *pDB = new Instance();
+  std::vector<String> iSQLVec = {
+      "SHOW TABLES;", "CREATE TABLE A(A INT, B FLOAT, C VARCHAR(30), D FLOAT);",
+      "SHOW TABLES;", "INSERT INTO A VALUES(4, 4.5, test_string, 5.0)",
+      "DROP TABLE A;"};
+  for (const auto &sSQL : iSQLVec) {
+    Execute(pDB, sSQL);
+    printf("%s Finish\n", sSQL.c_str());
+  }
+  delete pDB;
+}
+
+}  // namespace thdb
--- test/lab1/table_test.cc
+++ test/lab1/table_test.cc
@@ -2,15 +2,18 @@
 #include "gtest/gtest.h"
 #include "system/instance.h"
 
-using namespace thdb;
+namespace thdb {
 
-TEST(Lab1, SQLTest) {
+TEST(Lab1, TableTest) {
   Instance *pDB = new Instance();
   std::vector<String> iSQLVec = {
-      "SHOW TABLES;", "CREATE TABLE A(A INT, B FLOAT);", "SHOW TABLES;"};
+      "SHOW TABLES;", "CREATE TABLE A(A INT, B FLOAT, C VARCHAR(30), D FLOAT);",
+      "SHOW TABLES;", "DROP TABLE A;"};
   for (const auto &sSQL : iSQLVec) {
     Execute(pDB, sSQL);
     printf("%s Finish\n", sSQL.c_str());
   }
   delete pDB;
 }
+
+}  // namespace thdb
