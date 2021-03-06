65ca7980dd5d8fc5b885e6728db9948fbd6ddfc1
rcy <rcycyzh@163.com>
2021-03-06 18:32:46 +0800

    Update message in test.sh

--- test.sh
+++ test.sh
@@ -27,6 +27,6 @@ then
     echo -e "\x1b[1;32mStart to send result\x1b[0m" 
     curl -s -F "project=${CI_PROJECT_NAME}" -F "pipeline=${CI_PIPELINE_ID}" \
      -F "job=${CI_JOB_ID}" -F "lab=${CURRENT_LAB}" -F "file=@report.json" \
-     172.6.31.11:9876/collect/ || { echo -e "\x1b[1;31mFailed to send result\x1b[0m"; exit 1; }
+     172.6.31.11:9876/collect/ || { echo -e "\x1b[1;31mFailed to send result, please contact TA\x1b[0m"; exit 1; }
     echo  -e "\x1b[1;32mResult has been sent\x1b[0m"
 fi
