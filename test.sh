d074349d971ccab54e1f3f5a9941511b765fd5c0
rcy <rcycyzh@163.com>
2021-03-06 11:59:00 +0800

    Adjust order

--- test.sh
+++ test.sh
@@ -19,17 +19,17 @@ fi
 echo -e "\x1b[1;32mStarting test Lab${CURRENT_LAB}\x1b[0m"
 test/lab${CURRENT_LAB}_test --gtest_output="json:report.json" --gtest_color="yes" || fail=1
 
-if [ ${SEND_RESULT:-0} -eq 1 -a -e report.json ]
-then
-    curl -s -F "project=${CI_PROJECT_NAME}" -F "pipeline=${CI_PIPELINE_ID}" \
-     -F "job=${CI_JOB_ID}" -F "lab=${CURRENT_LAB}" -F "file=@report.json" \
-     172.6.31.11:9876/collect/ || { echo -e "\x1b[1;31mFailed to send result\x1b[0m"; exit 1; }
-    echo finished sending result
-fi
-
 if [ ${fail:-0} -eq 1 ]
 then
     exit 1
 fi
  
 echo -e "\x1b[1;32mLab${CURRENT_LAB} test passed\x1b[0m"
+
+if [ ${SEND_RESULT:-0} -eq 1 -a -e report.json ]
+then
+    curl -s -F "project=${CI_PROJECT_NAME}" -F "pipeline=${CI_PIPELINE_ID}" \
+     -F "job=${CI_JOB_ID}" -F "lab=${CURRENT_LAB}" -F "file=@report.json" \
+     172.6.31.11:9876/collect/ || { echo -e "\x1b[1;31mFailed to send result\x1b[0m"; exit 1; }
+    echo  -e "\x1b[1;32mLabfinished sending result\x1b[0m"
+fi
