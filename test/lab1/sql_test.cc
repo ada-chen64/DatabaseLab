1d781e105df7aacce819baecc64987beaad960ff
李文博 <li-wb17@mails.tsinghua.edu.cn>
2021-03-06 17:00:03 +0800

    Add sql test

--- test/lab1/sql_test.cc
+++ test/lab1/sql_test.cc
@@ -80,4 +80,26 @@ TEST(Lab1, UpdateDeleteTest) {
   delete pDB;
 }
 
+TEST(Lab1, DeleteInsertTest) {
+  Instance *pDB = new Instance();
+  std::vector<String> iSQLVec = {
+      "CREATE TABLE Persons(ID INT, FirstName VARCHAR(20), LastName VARCHAR(20), Temperature FLOAT);",
+      "INSERT INTO Persons VALUES(1, 'James', 'Smith', 36.4);",   // NOLINT
+      "INSERT INTO Persons VALUES(2, 'Maria', 'Garcia', 36.7);",  // NOLINT
+      "DELETE FROM Persons WHERE Persons.ID = 1;",                // NOLINT
+      "INSERT INTO Persons VALUES(3, 'Robert', 'Smith', 36.8);",  // NOLINT
+      "SELECT * FROM Persons;",                                   // NOLINT
+      "DROP TABLE Persons;"                                       // NOLINT
+  };
+  // clang-format off
+  std::vector<String> results = {"1\n", "1\n", "1\n", "1\n", "1\n", "3,Robert,Smith,36.8\n2,Maria,Garcia,36.7\n", "1\n"};
+  // clang-format on
+  for (uint32_t i = 0; i < iSQLVec.size(); i++) {
+    std::vector<Result *> iResVec = Execute(pDB, iSQLVec[i]);
+    EXPECT_EQ(iResVec.size(), 1);
+    EXPECT_EQ(iResVec[0]->ToString(), results[i]);
+  }
+  delete pDB;
+}
+
 }  // namespace thdb
