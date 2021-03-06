c4da6076628d3a7af2793e4473186060d6a7a567
董昊文 <dhw17@mails.tsinghua.edu.cn>
2021-03-06 16:43:38 +0800

    Add ;.

--- test/lab1/insert_test.cc
+++ test/lab1/insert_test.cc
@@ -10,28 +10,27 @@ namespace thdb {
 
 TEST(Lab1, InsertTest) {
   Instance *pDB = new Instance();
-  std::vector<String> iSQLVec = {
-      "SHOW TABLES;",
-      "CREATE TABLE A(A INT, Bsdfdsfdsfs FLOAT, C VARCHAR(30), D FLOAT);",
-      "SHOW TABLES;",
-      "INSERT INTO A VALUES(4, 4.5, 'test_string', 5.0)",
-      "INSERT INTO A VALUES(3, 4.5, 'test', 5.0)",
-      "INSERT INTO A VALUES(6, 4.5, 'string', 5.0)",
-      "SELECT * FROM A;",
-      "SELECT * FROM A WHERE A.A = 4;",
-      "SELECT * FROM A WHERE A.A < 4;",
-      "SELECT * FROM A WHERE A.A > 4;",
-      "SELECT * FROM A WHERE A.A <= 4;",
-      "SELECT * FROM A WHERE A.A >= 4;",
-      "SELECT * FROM A WHERE A.A <> 4;",
-      "SELECT * FROM A WHERE A.A <> 3;",
-      "SELECT * FROM A WHERE A.A < 5;",
-      "SELECT * FROM A WHERE A.A > 3;",
-      "INSERT INTO A VALUES(5, 4.5, 'test_string', 5.0)",
-      "SELECT * FROM A;",
-      "DELETE FROM A WHERE A.A = 4;",
-      "SELECT * FROM A;",
-      "DROP TABLE A;"};
+  std::vector<String> iSQLVec = {"SHOW TABLES;",
+                                 "CREATE TABLE A(A INT, Bsdfdsfdsfs FLOAT, C VARCHAR(30), D FLOAT);",
+                                 "SHOW TABLES;",
+                                 "INSERT INTO A VALUES(4, 4.5, 'test_string', 5.0);",
+                                 "INSERT INTO A VALUES(3, 4.5, 'test', 5.0);",
+                                 "INSERT INTO A VALUES(6, 4.5, 'string', 5.0);",
+                                 "SELECT * FROM A;",
+                                 "SELECT * FROM A WHERE A.A = 4;",
+                                 "SELECT * FROM A WHERE A.A < 4;",
+                                 "SELECT * FROM A WHERE A.A > 4;",
+                                 "SELECT * FROM A WHERE A.A <= 4;",
+                                 "SELECT * FROM A WHERE A.A >= 4;",
+                                 "SELECT * FROM A WHERE A.A <> 4;",
+                                 "SELECT * FROM A WHERE A.A <> 3;",
+                                 "SELECT * FROM A WHERE A.A < 5;",
+                                 "SELECT * FROM A WHERE A.A > 3;",
+                                 "INSERT INTO A VALUES(5, 4.5, 'test_string', 5.0);",
+                                 "SELECT * FROM A;",
+                                 "DELETE FROM A WHERE A.A = 4;",
+                                 "SELECT * FROM A;",
+                                 "DROP TABLE A;"};
   for (const auto &sSQL : iSQLVec) {
     std::vector<Result *> results = Execute(pDB, sSQL);
     PrintTable(results);
