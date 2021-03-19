d0a42e45e096c1c74710a083fbfa16229381032e
董昊文 <dhw17@mails.tsinghua.edu.cn>
2021-03-19 15:02:13 +0800

    Debug

--- test/lab2/node_page_test.cc
+++ test/lab2/node_page_test.cc
@@ -11,17 +11,17 @@ namespace thdb {
 void Search(NodePage* pNode, int nLow, int nHigh) {
   Field* pLow = new IntField(nLow);
   Field* pHigh = new IntField(nHigh);
-  printf("%u %u ", nLow, nHigh);
   auto iRes = pNode->Range(pLow, pHigh);
-  delete pLow;
-  delete pHigh;
+  printf("Range[%d,%d]: ", nLow, nHigh);
+  for (const auto& it : iRes) printf("%u.%u ", it.first, it.second);
 }
 
 TEST(Lab2, NodePageTest) {
-  NodePage* pNode = new NodePage(4, FieldType::INT_TYPE);
+  NodePage* pNode = new NodePage(4, FieldType::INT_TYPE, true);
   PageID nPageID = pNode->GetPageID();
   for (int i = 3; i < 7; ++i) {
     pNode->Insert(new IntField(i), PageSlotID(0, i));
+    printf("Insert %d\n", i);
   }
   for (int i = 0; i < 10; ++i) {
     Search(pNode, i, i);
