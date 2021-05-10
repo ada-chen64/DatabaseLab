4279bf8e69a4802049b039f1fb901a34966969be
wenbo <li-wb17@mails.tsinghua.edu.cn>
2021-05-10 11:21:29 +0800

    Update tests

--- test/lab4/mvcc_test.cc
+++ test/lab4/mvcc_test.cc
@@ -10,46 +10,48 @@ namespace thdb {
 
 class MVCCTests : public ::testing::Test {
  public:
+  // This function is called before every test.
   void SetUp() override {
     db = new Instance();
     std::vector<Column> columns({Column(column_name, FieldType::INT_TYPE)});
     Schema schema(columns);
-    db->CreateTable(table_name, schema);
-    table = db->GetTable(table_name);
+    db->CreateTable(table_name, schema, true);
     txn_manager = db->GetTransactionManager();
-    insert_record = table->EmptyRecord();
-    insert_record->Build({record_init});
   }
 
+  // This function is called after every test
   void TearDown() override {
-    delete insert_record;
     db->DropTable(table_name);
     delete db;
   }
 
   void CheckExist(Transaction *txn, String record_string) {
-    std::vector<PageSlotID> rids = table->SearchRecord(nullptr, txn);
+    std::vector<PageSlotID> rids = db->Search(table_name, nullptr, {}, txn);
     EXPECT_EQ(rids.size(), 1);
-    Record *select_record = table->GetRecord(rids[0].first, rids[0].second);
-    EXPECT_EQ(select_record->ToString(), record_string);
-    delete select_record;
+    // Record *select_record = db->GetRecord(table_name, rids[0]);
+    // EXPECT_EQ(select_record->ToString(), record_string);
+    // delete select_record;
   }
 
   void CheckNotExist(Transaction *txn) {
-    std::vector<PageSlotID> rids = table->SearchRecord(nullptr, txn);
-    EXPECT_EQ(rids.size(), 0);
+    std::vector<PageSlotID> rids = db->Search(table_name, nullptr, {}, txn);
+    std::vector<Record *> records;
+    for (const auto &rid : rids) {
+      records.push_back(db->GetRecord(table_name, rid));
+    }
+    EXPECT_EQ(records.size(), 0);
   }
 
   Instance *db;
-  Table *table;
   TransactionManager *txn_manager;
-  Record *insert_record;
-  // Record 的 ToString 函数在字符串末添加了一个空格
   const String record_init = "0 ";
   const String record_updated = "1 ";
 
- private:
+  const std::vector<String> record_build = {"0"};
+
   const String table_name = "test";
+
+ private:
   const String column_name = "mvcc";
 };
 
@@ -73,7 +75,7 @@ class MVCCTests : public ::testing::Test {
 TEST_F(MVCCTests, CommitInsert1) {
   auto *txn0 = txn_manager->Begin();
 
-  table->InsertRecord(insert_record, txn0);
+  db->Insert(table_name, record_build, txn0);
 
   CheckExist(txn0, record_init);
 
@@ -119,7 +121,7 @@ TEST_F(MVCCTests, CommitInsert2) {
 
   auto *txn1 = txn_manager->Begin();
 
-  table->InsertRecord(insert_record, txn1);
+  db->Insert(table_name, record_build, txn1);
 
   CheckNotExist(txn0);
 
@@ -161,7 +163,7 @@ TEST_F(MVCCTests, CommitInsert2) {
 TEST_F(MVCCTests, AbortInsert1) {
   auto *txn0 = txn_manager->Begin();
 
-  table->InsertRecord(insert_record, txn0);
+  db->Insert(table_name, record_build, txn0);
 
   CheckExist(txn0, record_init);
 
@@ -207,7 +209,7 @@ TEST_F(MVCCTests, AbortInsert2) {
 
   auto *txn1 = txn_manager->Begin();
 
-  table->InsertRecord(insert_record, txn1);
+  db->Insert(table_name, record_build, txn1);
 
   CheckNotExist(txn0);
 
