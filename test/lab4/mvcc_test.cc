23af5eefe6f22c264b14e67b94975cacb1b72f60
wenbo <li-wb17@mails.tsinghua.edu.cn>
2021-05-10 16:38:45 +0800

    Add update and delete test

--- test/lab4/mvcc_test.cc
+++ test/lab4/mvcc_test.cc
@@ -4,6 +4,7 @@
 #include "backend/backend.h"
 #include "gtest/gtest.h"
 #include "record/fixed_record.h"
+#include "record/transform.h"
 #include "table/schema.h"
 
 namespace thdb {
@@ -28,30 +29,30 @@ class MVCCTests : public ::testing::Test {
   void CheckExist(Transaction *txn, String record_string) {
     std::vector<PageSlotID> rids = db->Search(table_name, nullptr, {}, txn);
     EXPECT_EQ(rids.size(), 1);
-    // Record *select_record = db->GetRecord(table_name, rids[0]);
-    // EXPECT_EQ(select_record->ToString(), record_string);
-    // delete select_record;
+    Record *select_record = db->GetRecord(table_name, rids[0], txn);
+    EXPECT_EQ(select_record->ToString(), record_string);
+    delete select_record;
   }
 
   void CheckNotExist(Transaction *txn) {
     std::vector<PageSlotID> rids = db->Search(table_name, nullptr, {}, txn);
     std::vector<Record *> records;
     for (const auto &rid : rids) {
-      records.push_back(db->GetRecord(table_name, rid));
+      records.push_back(db->GetRecord(table_name, rid, txn));
     }
     EXPECT_EQ(records.size(), 0);
   }
 
   Instance *db;
   TransactionManager *txn_manager;
-  const String record_init = "0 ";
-  const String record_updated = "1 ";
+  std::vector<Transform> transform = {Transform(0, FieldType::INT_TYPE, "1")};
 
   const std::vector<String> record_build = {"0"};
 
-  const String table_name = "test";
+  const String record_init = "0 ";
+  const String record_updated = "1 ";
 
- private:
+  const String table_name = "test";
   const String column_name = "mvcc";
 };
 
@@ -226,6 +227,104 @@ TEST_F(MVCCTests, AbortInsert2) {
   auto *txn2 = txn_manager->Begin();
 
   CheckNotExist(txn2);
+
+  txn_manager->Commit(txn2);
+  delete txn2;
+}
+
+//    Txn #0 | Txn #1 | Txn #2 |
+//    --------------------------
+//    BEGIN  |        |        |
+//    W(X)   |        |        |
+//    R(X)   |        |        |
+//           | BEGIN  |        |
+//           | R(X)   |        |
+//    COMMIT |        |        |
+//           | R(X)   |        |
+//           | COMMIT |        |
+//           |        | BEGIN  |
+//           |        | R(X)   |
+//           |        | COMMIT |
+//
+// Txn #0 should only read Txn #0's version of X
+// Txn #1 should only read the previous version of X because its start time is before #0's commit
+// Txn #2 should only read Txn #0's version of X
+TEST_F(MVCCTests, DISABLED_CommitUpdate1) {
+  // Insert the tuple to be Updated later
+  auto *txn = txn_manager->Begin();
+  db->Insert(table_name, record_build, txn);
+  txn_manager->Commit(txn);
+
+  auto *txn0 = txn_manager->Begin();
+
+  db->Update(table_name, nullptr, {}, transform, txn);
+
+  CheckExist(txn0, record_updated);
+
+  auto *txn1 = txn_manager->Begin();
+
+  CheckExist(txn1, record_init);
+
+  txn_manager->Commit(txn0);
+  delete txn0;
+
+  CheckExist(txn1, record_init);
+
+  txn_manager->Commit(txn1);
+  delete txn1;
+
+  auto *txn2 = txn_manager->Begin();
+
+  CheckExist(txn2, record_updated);
+
+  txn_manager->Commit(txn2);
+  delete txn2;
+}
+
+//    Txn #0 | Txn #1 | Txn #2 |
+//    --------------------------
+//    BEGIN  |        |        |
+//    W(X)   |        |        |
+//    R(X)   |        |        |
+//           | BEGIN  |        |
+//           | R(X)   |        |
+//    COMMIT |        |        |
+//           | R(X)   |        |
+//           | COMMIT |        |
+//           |        | BEGIN  |
+//           |        | R(X)   |
+//           |        | COMMIT |
+//
+// Txn #0 should only read Txn #0's version of X
+// Txn #1 should only read the previous version of X because its start time is before #0's commit
+// Txn #2 should only read Txn #0's version of X
+TEST_F(MVCCTests, DISABLED_CommitDelete1) {
+  // Insert the tuple to be Deleted later
+  auto *txn = txn_manager->Begin();
+  db->Insert(table_name, record_build, txn);
+  txn_manager->Commit(txn);
+
+  auto *txn0 = txn_manager->Begin();
+
+  db->Delete(table_name, nullptr, {}, txn);
+
+  CheckNotExist(txn0);
+
+  auto *txn1 = txn_manager->Begin();
+
+  CheckExist(txn1, record_init);
+
+  txn_manager->Commit(txn0);
+  delete txn0;
+
+  CheckExist(txn1, record_init);
+
+  txn_manager->Commit(txn1);
+  delete txn1;
+
+  auto *txn2 = txn_manager->Begin();
+
+  CheckNotExist(txn0);
 
   txn_manager->Commit(txn2);
   delete txn2;
