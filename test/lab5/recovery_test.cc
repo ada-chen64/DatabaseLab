3708ba2292faa0d98446b6310417399bc0da4e54
wenbo <li-wb17@mails.tsinghua.edu.cn>
2021-05-15 21:05:40 +0800

    Add initial lab5 test

---
+++ test/lab5/recovery_test.cc
@@ -0,0 +1,74 @@
+#include "backend/backend.h"
+#include "gtest/gtest.h"
+
+namespace thdb {
+
+class RecoveryTests : public ::testing::Test {
+ public:
+  // This function is called before every test.
+  void SetUp() override {
+    db = new Instance();
+    std::vector<Column> columns({Column(column_name, FieldType::INT_TYPE)});
+    Schema schema(columns);
+    db->CreateTable(table_name, schema, true);
+  }
+
+  // This function is called after every test
+  void TearDown() override {
+    db->DropTable(table_name);
+    delete db;
+  }
+
+  void CheckExist(Transaction *txn, String record_string) {
+    std::vector<PageSlotID> rids = db->Search(table_name, nullptr, {}, txn);
+    EXPECT_EQ(rids.size(), 1);
+    Record *select_record = db->GetRecord(table_name, rids[0], txn);
+    EXPECT_EQ(select_record->ToString(), record_string);
+    delete select_record;
+  }
+
+  void CheckNotExist(Transaction *txn) {
+    std::vector<PageSlotID> rids = db->Search(table_name, nullptr, {}, txn);
+    std::vector<Record *> records;
+    for (const auto &rid : rids) {
+      records.push_back(db->GetRecord(table_name, rid, txn));
+    }
+    EXPECT_EQ(records.size(), 0);
+  }
+
+  Instance *db;
+
+  const std::vector<String> record_build = {"0"};
+
+  const String record_init = "0 ";
+
+  const String table_name = "test";
+  const String column_name = "recovery";
+};
+
+TEST_F(RecoveryTests, RedoTest) {
+  Transaction *txn = db->GetTransactionManager()->Begin();
+  db->Insert(table_name, record_build, txn);
+  db->GetTransactionManager()->Commit(txn);
+  delete txn;
+
+  delete db;
+
+  db = new Instance();
+  txn = db->GetTransactionManager()->Begin();
+  CheckExist(txn, record_init);
+}
+
+TEST_F(RecoveryTests, UndoTest) {
+  Transaction *txn = db->GetTransactionManager()->Begin();
+  db->Insert(table_name, record_build, txn);
+  delete txn;
+
+  delete db;
+
+  db = new Instance();
+  txn = db->GetTransactionManager()->Begin();
+  CheckNotExist(txn);
+}
+
+}  // namespace thdb
